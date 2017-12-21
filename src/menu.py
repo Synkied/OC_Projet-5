68b78342cf635272e2b306bbdd951285a7cbd717
Synkied <synkx@hotmail.fr>
2017-12-21 22:36:03 +0100

    new menu funcs

--- src/menu.py
+++ src/menu.py
@@ -27,23 +27,28 @@ de remplacer un produit par un autre plus "healthy" ;) !""".upper()
         print("0 - Quitter l'application.")
         print()
         print("Saisissez votre choix : ")
-        answer = input(" >> ")
+        try:
+            answer = input(" >> ")
 
-        if answer == "1":
-            self.menu_actions[answer](self)
+            if answer == "1":
+                self.menu_actions[answer](self)
 
-        elif answer == "2":
-            self.menu_actions[answer](self)
+            elif answer == "2":
+                self.menu_actions[answer](self)
 
-        elif answer == "0":
-            self.menu_actions[answer](self)
+            elif answer == "0":
+                self.menu_actions[answer](self)
 
-        else:
-            print()
-            print("!" * 31)
-            print("Veuillez faire un choix valide.")
-            print("!" * 31)
-            self.menu_actions['main_menu'](self)
+            else:
+                print()
+                print("!" * 31)
+                print("Veuillez faire un choix valide.")
+                print("!" * 31)
+                self.menu_actions['main_menu'](self)
+
+        except KeyboardInterrupt as keyinter:
+            print("Au revoir, alors ! :)")
+            exit()
 
     def quit(self):
         print("Bye bye !")
@@ -58,75 +63,119 @@ de remplacer un produit par un autre plus "healthy" ;) !""".upper()
         print()
         print("Voici toutes les catégories, veuillez en choisir une : ")
         print("-" * 54)
-        select_categories = Categories.select().order_by(Categories.id)
-        categories_ids = []
-        for category in select_categories:
-            # put all categories ids in a list to iterate over...
-            categories_ids.append(category.id)
-            print(category.id, "-", category.name)
-        print()
-        cat_choice = input(" >> ")
-
         try:
-            cat_choice = int(cat_choice)
-
-            if cat_choice in categories_ids:
-                self.display_products(cat_choice)
-            else:
-                print("!" * 36)
-                print("Veuillez entrer une catégorie valide.")
-                print("!" * 36)
-                self.display_categories()
-        except ValueError as VE:
+            select_categories = Categories.select().order_by(Categories.id)
+            categories_ids = []
+            for category in select_categories:
+                # put all categories ids in a list to iterate over...
+                categories_ids.append(category.id)
+                print(category.id, "-", category.name)
             print()
-            print("!" * 26)
-            print("Veuillez entrer un nombre.")
-            print("!" * 26)
-            self.display_categories()
+            cat_choice = input(" >> ")
 
-    def display_products(self, cat_choice):
+            try:
+                cat_choice = int(cat_choice)
+
+                if cat_choice in categories_ids:
+                    self.display_products(cat_choice, 1, 10)
+                else:
+                    print("!" * 36)
+                    print("Veuillez entrer une catégorie valide.")
+                    print("!" * 36)
+                    self.display_categories()
+
+            except ValueError as VE:
+                print()
+                print("!" * 26)
+                print("Veuillez entrer un nombre.")
+                print("!" * 26)
+                self.display_categories()
+
+        except KeyboardInterrupt as keyinter:
+            print("Au revoir, alors ! :)")
+            exit()
+
+    def display_products(self, cat_choice, page, products_per_page):
         """
         display products in the terminal.
         cat_choice is the category selected by the user.
         """
         print()
         print("Voici les produits, veuillez en choisir un : ")
+        print("Page suivante (n), page précédente (p)")
         print("-" * 54)
-        query = Products.select().where(
-            Products.cat_id == cat_choice
-        ).order_by(
-            Products.id
-        ).paginate(1, 10)
+        try:
+            query = Products.select().where(
+                Products.cat_id == cat_choice
+            ).order_by(
+                Products.id
+            )
 
-        products_ids = []
-        for product in query:
-            # get all products ids in a list to iterate over...
-            products_ids.append(product.id)
-            print(product.id, "-", product.name)
-        print()
-        prod_choice = input(" >> ")
+            # query pagination
+            query_paginated = query.paginate(page, products_per_page)
 
-        if prod_choice.lower() == "n" or prod_choice.lower() == "p":
-            # get next or previous page if asked by user
-            self.menu_actions[prod_choice](self, cat_choice)
+            # an empty list to store all filtered products id
+            products_ids = []
+            num_products = [product.id for product in query]
 
-        else:
-            try:
-                prod_choice = int(prod_choice)
-                if prod_choice in products_ids:
-                    self.display_better_product(prod_choice)
-                else:
+            for product in query_paginated:
+                # get all products ids in a list to iterate over...
+                products_ids.append(product.id)
+                print(product.id, "-", product.name)
+
+            num_of_pages = len(num_products) // products_per_page
+
+            print("*" * 25)
+            print("Page", page, "/", num_of_pages)
+
+            print()
+            prod_choice = input(" >> ")
+
+            if prod_choice.lower() == "n" or prod_choice.lower() == "p":
+                # get next or previous page if asked by user
+                self.menu_actions[prod_choice](
+                    self,
+                    cat_choice,
+                    page,
+                    products_per_page,
+                )
+
+            else:
+                try:
+                    prod_choice = int(prod_choice)
+                    if prod_choice in products_ids:
+                        self.display_better_product(prod_choice)
+                    else:
+                        print("!" * 36)
+                        print("Veuillez entrer un produit valide.")
+                        print("!" * 36)
+                        self.display_products(
+                            cat_choice,
+                            page,
+                            products_per_page,
+                        )
+
+                except KeyboardInterrupt as keyinter:
+                    print("Au revoir, alors ! :)")
+                    exit()
+
+                except ValueError as VE:
+                    print()
                     print("!" * 36)
-                    print("Veuillez entrer un produit valide.")
+                    print("Veuillez entrer un nombre ou (n/p).")
                     print("!" * 36)
-                    self.display_products(cat_choice)
+                    self.display_products(
+                        cat_choice,
+                        page,
+                        products_per_page,
+                    )
 
-            except ValueError as VE:
-                print()
-                print("!" * 36)
-                print("Veuillez entrer un nombre ou (n/p).")
-                print("!" * 36)
-                self.display_products(cat_choice)
+        except KeyboardInterrupt as keyinter:
+            print("Au revoir, alors ! :)")
+            exit()
+
+        # returns query to be able to display next and previous pages
+        return query, num_of_pages
 
     def display_better_product(self, prod_choice):
         # get nutri grade of chosen product
@@ -134,11 +183,22 @@ de remplacer un produit par un autre plus "healthy" ;) !""".upper()
             Products.id == prod_choice
         )
 
-        substitute_query = Products.select().where(
-            Products.nutri_grade <= cur_product.nutri_grade,
-            Products.cat_id == cur_product.cat_id).order_by(
-            fn.Rand()
-        ).limit(1)
+        # if nutri_grade = a, give a similar grade product
+        # else give a product with a better nutrigrade
+        if cur_product.nutri_grade == "a":
+            substitute_query = Products.select().where(
+                Products.nutri_grade == cur_product.nutri_grade,
+                Products.cat_id == cur_product.cat_id).order_by(
+                fn.Rand()
+            ).limit(1)
+
+        else:
+            substitute_query = Products.select().where(
+                Products.nutri_grade < cur_product.nutri_grade,
+                Products.cat_id == cur_product.cat_id).order_by(
+                fn.Rand()
+            ).limit(1)
+
         for product in substitute_query:
             print()
             print("Voici le substitut proposé :")
@@ -160,13 +220,53 @@ de remplacer un produit par un autre plus "healthy" ;) !""".upper()
                 print("Magasin :", store.name)
             # OFF url
             print("URL OpenFoodFacts :", product.url)
+            print()
+            print("-" * 33)
+            print("Voulez-vous l'enregistrer ? (o/n)")
 
-    def next_page(self, cat_choice):
-        print("page suivante")
-        self.display_products(cat_choice).query.paginate(2, 10)
+            save_fav = input(" >> ")
 
-    def previous_page(self):
-        print("page précédente")
+            if save_fav.lower() == "o":
+                pass
+
+            elif save_fav.lower() == "n":
+                pass
+
+            else:
+                print('Veuillez entrer "o" ou "n"')
+
+    def next_page(self, cat_choice, page, products_per_page):
+        """
+        Gets the next page if it exists
+        """
+        page += 1
+        query, num_of_pages = self.display_products(cat_choice, page, products_per_page)
+
+        query.paginate(
+            page,
+            products_per_page,
+        )
+
+    def previous_page(self, cat_choice, page, products_per_page):
+        """
+        If the current page isn't 1, gets previous page.
+        """
+        if page > 1:
+            page -= 1
+            query, num_of_pages = self.display_products(cat_choice, page, products_per_page)
+
+            query.paginate(
+                page,
+                products_per_page,
+            )
+        else:
+            print("Vous êtes à la première page ;). ")
+            query, num_of_pages = self.display_products(cat_choice, page, products_per_page)
+
+            query.paginate(
+                page,
+                products_per_page,
+            )
 
     def add_favs(self):
         pass
